60f3deffd45a6525b583218fc1d5920ed21952b5
vijaymkamble <38262660+vijaymkamble@users.noreply.github.com>
2021-11-17 22:51:07 +0530

    Update GitClass.java
    companyWelcome() method has been updated

--- GitTestProject/src/com/testGit/GitClass.java
+++ GitTestProject/src/com/testGit/GitClass.java
@@ -5,6 +5,7 @@ public class GitClass
 	public static void companyWelcome(String msg)
 	{
 		System.out.println("Wel-come to the FIS family dear "+msg);
+		System.out.println("And you will be get laptop this week only.......!!");
 	}
 
 	public static void main(String[] args) {
