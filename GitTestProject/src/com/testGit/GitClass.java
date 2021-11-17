3dcc4d6e5a7493200219fed064f689f4ff41b6c6
Vijay <vijaymkamble1994@gmail.com>
2021-11-17 22:42:14 +0530

    Adding the GitClass java file into the project

---
+++ GitTestProject/src/com/testGit/GitClass.java
@@ -0,0 +1,13 @@
+package com.testGit;
+
+public class GitClass 
+{
+	public static void companyWelcome(String msg)
+	{
+		System.out.println("Wel-come to the FIS family dear "+msg);
+	}
+
+	public static void main(String[] args) {
+		companyWelcome("Vijay Kamble");
+	}
+}
--- GitTestProject/src/module-info.java
+++ GitTestProject/src/module-info.java
@@ -6,5 +6,5 @@
  *
  */
 module GitTestProject {
-	//testing
+	
 }
